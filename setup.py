a0517ae43828eeeaf5269abc56067020da744ef9
Felix Reichel <f.reichel@gmx.at>
2024-06-15 20:23:49 +0200

    Fix release publish job

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='alpha-miner',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     author='Felix Reichel',
     author_email='kontakt@felixreichel.com',
